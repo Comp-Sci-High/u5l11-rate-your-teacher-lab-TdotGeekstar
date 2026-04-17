1d3790ea599259594b797fbc6b85f27a98f38dc1
TdotGeekstar <tom.saoura26@compscihigh.org>
2026-04-17 19:07:22 +0000

    Finished

--- veiws/teacherScript.js
+++ veiws/teacherScript.js
@@ -1,6 +1,5 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
-
 const createForm = document.querySelector("form");
 
 createForm.addEventListener("submit", async (e) => {
@@ -9,14 +8,13 @@ createForm.addEventListener("submit", async (e) => {
   const teacherData = new FormData(createForm);
   const reqBody = Object.fromEntries(teacherData);
 
-  await fetch("/teachers", {
+  await fetch("/add/teacher", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(reqBody),
+    body: JSON.stringify(reqBody)
   });
 
-  
   window.location.href = "/teachers";
 });
